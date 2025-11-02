eee42720fa31e816967e1bf5f6c10e94133052a4
Aaliyah Sayed <aaliyah.sayed@gmail.com>
2025-11-02 10:55:25 -0500

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Aaliyah Sayed on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloTests/HelloTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTests.swift
+//  HelloTests
+//
+//  Created by Aaliyah Sayed on 11/2/25.
+//
+
+import Testing
+@testable import Hello
+
+struct HelloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
